6e23210d2c15c1914483568a64fc660cfb50de8f
Rafael Crawford <crawfordmg13@gmail.com>
2025-03-02 19:28:55 -0300

    Mudanças feitas em sis1 e sis2 formatação da data, finalização do merge

--- merge.py
+++ merge.py
@@ -2,12 +2,17 @@ import sistema1, sistema2
 import pandas as pd
 
 
-merge_df = pd.merge(sistema1.sis1_df, sistema2.sis2_df, how='inner', on=['Filial', 'Descrição'])
+sistema1.sis1_df['Filial_Codigo'] = sistema1.sis1_df['Filial_Codigo'].astype(str)
+sistema2.sis2_df['Filial_Codigo'] = sistema2.sis2_df['Filial_Codigo'].astype(str)
 
-merge_df['Diferença'] = merge_df['Valor_x'] - ['Valor_y']
+merge_df = pd.merge(sistema1.sis1_df, sistema2.sis2_df, how='inner', on=['Filial_Codigo', 'Descrição'])
 
 merge_df.rename(columns={
-    'Valor_x':'Valor Sistema 1',
-    'Valor_y': 'Valor Sistema 2'},inplace=True)
+    'Valor_x':'Total Sis 1',
+    'Valor_y': 'Total Sis 2'},inplace=True)
+
+merge_df['Diferença'] = round(merge_df['Total Sis 1'] - merge_df['Total Sis 2'],2)
+merge_df.rename(columns={'Filial_Codigo':'ID'}, inplace=True)
+merge_df = merge_df[['ID','Filial','Descrição','Total Sis 1','Total Sis 2','Diferença','Data']]
 
 print(merge_df)
--- sistema2.py
+++ sistema2.py
@@ -7,8 +7,10 @@ sis2 = pd.read_excel('data/Folha Pag_sis2.xlsx', header=1)
 tabela2 = sis2[['Filial', 'Data Lcto', 'Valor', 'Hist Lanc']]
 
 sis2_df = sis2.groupby(['Filial', 'Data Lcto', 'Hist Lanc'])['Valor'].sum().reset_index()
-sis2_df.rename(columns={'Hist Lanc': 'Descrição','Valor':'Total Sis 2','Data Lcto':'Data'}, inplace=True)
+sis2_df.rename(columns={'Filial':'Filial_Codigo','Hist Lanc': 'Descrição','Valor':'Total Sis 2','Data Lcto':'Data'}, inplace=True)
+sis2_df['Data'] = pd.to_datetime(sis2_df['Data'], dayfirst=True).dt.strftime('%d-%m-%Y')
 
-print(sis2_df)
+if __name__ == "__main__":
+    print(sis2_df)
 
 
